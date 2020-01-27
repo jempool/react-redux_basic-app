23373d73656fb02a03815df4b137e4fc1204fe39
jem <jem.suarez@correounivalle.edu.co>
2020-01-27 14:27:24 -0500

    react-redux without middleware

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import { render } from "react-dom";
+import { Provider } from "react-redux";
+import store from "./js/store/index";
+import App from "./js/components/App";
 
-import index from "./js/index"; // imported to be displayed in the browser console
-
-ReactDOM.render(<App />, document.getElementById('root'));
+render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
--- src/js/index.js
+++ src/js/index.js
@@ -3,4 +3,6 @@ import { addArticle } from "../js/actions/index";
 
 window.store = store;
 window.addArticle = addArticle;
+// store.getState();
+
 
